5cfee4d6a10eb5aa634109f2f71a5e776a70e405
João Mateus <jms.lopes2001@gmail.com>
2023-08-21 10:45:11 -0300

    Initial commit

---
+++ src/main/java/com/projeto/api/bootCampsantander/BootCampSantanderApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.api.bootCampsantander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCampSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/api/bootCampsantander/BootCampSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.api.bootCampsantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCampSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
